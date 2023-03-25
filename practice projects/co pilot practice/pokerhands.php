ab29643a08b8bc18b698da61f0adc53b8c800752
Elijah Proctor <elijahred23@gmail.com>
2023-03-25 00:06:35 -0500

    added poker hands

---
+++ practice projects/co pilot practice/pokerhands.php
@@ -0,0 +1,69 @@
+<?php
+
+class Card{
+    private $suit;
+    private $value;
+    function __construct($suit, $value){
+        $this->suit = $suit;
+        $this->value = $value;
+    }
+    public function getSuit(){
+        return $this->suit;
+    }
+    public function getValue(){
+        return $this->value;
+    }
+};
+
+class Deck{
+    public $cards;
+
+    function __construct(){
+        $this->cards = [];
+        $suits = ['clubs', 'diamonds', 'hearts', 'spades'];
+        $values = ['ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'jack', 'queen', 'king'];
+        foreach($suits as $suit){
+            foreach($values as $value){
+                $this->cards[] = new Card($suit, $value);
+            }
+        }
+    }
+    public function shuffle(){
+        shuffle($this->cards);
+    }
+    public function showCards(){
+        foreach($this->cards as $card){
+            echo $card->getSuit() . " " . $card->getValue() . PHP_EOL;
+        }
+    }
+    public function getCards(){
+        return $this->cards;
+    }
+}
+
+class Hand{
+    private $cards;
+    function __construct($deck){
+        $this->cards = [];
+        for($i = 0; $i < 5; $i++){
+            $this->cards[] = array_pop($deck->cards);
+        }
+    }
+    public function showCards(){
+        echo "Your hand is:" . PHP_EOL;
+        foreach($this->cards as $card){
+            echo $card->getSuit() . " " . $card->getValue() . PHP_EOL;
+        }
+    }
+}
+
+
+
+$deck = new Deck();
+
+$deck->shuffle();
+$deck->showCards();
+
+$hand = new Hand($deck);
+$hand->showCards();
+
